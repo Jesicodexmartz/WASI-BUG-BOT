c7b47b0c4b90233b6aeb5766883ed2b637c716cc
farajidexmart <farajidexmart@gmail.com>
2024-07-28 07:47:15 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '255678292150'  //creator number
+global.ownername = ' FEILAN ' //owner name
+global.botname = 'FEILAN DE XMART ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
